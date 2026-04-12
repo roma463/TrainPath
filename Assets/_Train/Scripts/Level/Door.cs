a8f05faa7c23d905f238b9946cd599ddb9955c31
Roman <romaklassman@gmail.com>
2026-04-12 16:42:04 +0700

    doorAnimation

--- Assets/_Train/Scripts/Level/Door.cs
+++ Assets/_Train/Scripts/Level/Door.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using _Train.Scripts.Root;
 using UnityEngine;
 
@@ -8,11 +9,14 @@ namespace _Train.Scripts.Level
         [SerializeField] private InteractableObject interactableObject;
         [SerializeField] private Transform door;
         [SerializeField] private Vector3 openAngle;
+        [SerializeField] private float speedAnimation;
         [SerializeField] private string open, closed;
     
         private bool _doorOpen;
         private Quaternion _doorOpenRotation;
         private Quaternion _doorClosedRotation;
+        
+        private Coroutine _coroutine;
     
         private void Start()
         {
@@ -27,14 +31,37 @@ namespace _Train.Scripts.Level
 
         private void OnDestroy()
         {
+            StopAnimation();
             interactableObject.OnInteracting -= OnInteract;
         }
 
         private void OnInteract()
         {
             _doorOpen = !_doorOpen;
-            interactableObject.SetPromt(_doorOpen? closed : open);
-            door.localRotation = _doorOpen ? _doorOpenRotation : _doorClosedRotation;
+            
+            interactableObject.SetPromt(_doorOpen ? closed : open);
+            StopAnimation();
+            _coroutine = StartCoroutine(Animation(_doorOpen ? _doorOpenRotation : _doorClosedRotation));
+        }
+
+        private void StopAnimation()
+        {
+            if (_coroutine != null)
+            {
+                StopCoroutine(_coroutine);
+                _coroutine = null;
+            }
+        }
+
+        private IEnumerator Animation(Quaternion target)
+        {
+            while (door.localRotation != target)
+            {
+                door.localRotation = Quaternion.Lerp(door.localRotation, target, speedAnimation * Time.deltaTime);
+                yield return null;
+            }
+
+            _coroutine = null;
         }
     }
 }
